66b816c9740465b925b11282e25c7baa8e35804b
Ryuukae <ryuukae.dev@gmail.com>
2026-09-22 01:37:28 -0400

    refactor(data): update repositories data store with new asset paths

--- src/scripts/data/repositories.js
+++ src/scripts/data/repositories.js
@@ -1,21 +1,12 @@
 export const repositories = [
   {
-    name: 'Simple-FAQ-Page',
+    name: 'Interactive-Web-Quiz',
     description:
-      'A responsive FAQ component designed to showcase clean and minimalist aesthetics, adaptability across various screen sizes, and accessibility features.',
-    pagesLink: 'https://pages.ryuukae.io/simple-faq-page/src',
-    repoLink: 'https://www.github.com/ryuukae/simple-faq-page',
-    imageSrc: './src/resources/Simple FAQ Page Screenshot.png',
-    imageAlt: 'Screenshot of the Simple-FAQ-Page site.',
-  },
-  {
-    name: 'Advanced-FAQ-Page',
-    description:
-      'A responsive FAQ component designed to showcase clean and minimalist aesthetics, adaptability across various screen sizes, smooth animations for an enhanced user experience, and accessibility features. Key Features include a clean and minimalist design responsive layout for different screen sizes, smooth animations for improved user interaction, and accessibility considerations.',
-    pagesLink: 'https://pages.ryuukae.io/advanced-faq-page/src/index.html',
-    repoLink: 'https://www.github.com/ryuukae/advanced-faq-page',
-    imageSrc: './src/resources/Advanced FAQ Page Screenshot.jpg',
-    imageAlt: 'Screenshot of the Advanced-FAQ-Page site.',
+      'An enterprise-grade interactive quiz platform that features visual card creation, dual QAD/JSON data ingestion, and auto-expanding textareas. The application focuses on an intuitive user experience for quiz building and taking, utilizing modern web standards, and ensuring robustness through automated unit testing.',
+    pagesLink: 'https://pages.ryuukae.io/interactive-web-quiz',
+    repoLink: 'https://github.com/Ryuukae/interactive-web-quiz',
+    imageSrc: './src/resources/interactive-web-quiz-screenshot.png',
+    imageAlt: 'Screenshot of the interactive-web-quiz site.',
   },
   {
     name: 'Multi-step.Registration.Form',
@@ -23,8 +14,26 @@ export const repositories = [
       'A comprehensive solution for implementing a multi-step registration form, this project exemplifies best practices in creating efficient and user-friendly account creation processes for web applications. The implementation focuses on modular architecture, multi-screen design, real-time form validation, progressive tracking, and responsive UI, while incorporating accessibility features.',
     pagesLink: 'https://pages.ryuukae.io/Multi-step.Registration.Form/src/',
     repoLink: 'https://github.com/Ryuukae/Multi-step.Registration.Form',
-    imageSrc: './src/resources/Multi-Step%20Registration%20Form%20Screenshot.jpg',
+    imageSrc: './src/resources/multi-step-registration-form-screenshot.jpg',
     imageAlt: 'Screenshot of the Multi-step.Registration.Form site.',
+  },
+  {
+    name: 'Advanced-FAQ-Page',
+    description:
+      'A responsive FAQ component designed to showcase clean and minimalist aesthetics, adaptability across various screen sizes, smooth animations for an enhanced user experience, and accessibility features. Key Features include a clean and minimalist design responsive layout for different screen sizes, smooth animations for improved user interaction, and accessibility considerations.',
+    pagesLink: 'https://pages.ryuukae.io/advanced-faq-page/src/index.html',
+    repoLink: 'https://www.github.com/ryuukae/advanced-faq-page',
+    imageSrc: './src/resources/advanced-faq-page-screenshot.jpg',
+    imageAlt: 'Screenshot of the Advanced-FAQ-Page site.',
+  },
+  {
+    name: 'Simple-FAQ-Page',
+    description:
+      'A responsive FAQ component designed to showcase clean and minimalist aesthetics, adaptability across various screen sizes, and accessibility features.',
+    pagesLink: 'https://pages.ryuukae.io/simple-faq-page/src',
+    repoLink: 'https://www.github.com/ryuukae/simple-faq-page',
+    imageSrc: './src/resources/simple-faq-page-screenshot.png',
+    imageAlt: 'Screenshot of the Simple-FAQ-Page site.',
   },
   {
     name: 'To-Do.List',
@@ -32,7 +41,7 @@ export const repositories = [
       "A simple to-do list application, this project demonstrates basic task management functionality. It allows users to add, and delete tasks through an aesthetic interface, providing a straightforward interface for organizing daily activities. The page saves the user's tasks to local storage so that the users may keep their tasks upon leaving or refreshing the page.",
     pagesLink: 'https://pages.ryuukae.io/To-Do.List',
     repoLink: 'https://github.com/ryuukae/To-Do.List',
-    imageSrc: './src/resources/To-Do.List%20Screenshot.jpg',
+    imageSrc: './src/resources/to-do-list-screenshot.jpg',
     imageAlt: "Screenshot of the To-Do.List's site.",
   },
 ];
